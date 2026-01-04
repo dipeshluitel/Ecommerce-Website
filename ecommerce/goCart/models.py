66698758b894e53cedd153586a52fe73c96a4484
Dipesh <dipeshluitel40@gmail.com>
2026-01-04 22:09:13 +0545

    add(models): from admin panel 20 diffect objects are created

--- ecommerce/goCart/models.py
+++ ecommerce/goCart/models.py
@@ -15,7 +15,6 @@ class Product(models.Model):
     selling_price = models.FloatField()
     discounted_price = models.FloatField()
     description = models.TextField()
-    composition = models.TextField(default='')
     prodapp = models.TextField(default='')
     category = models.CharField(choices=CATEGORY_CHOICES , max_length=2)
     product_image = models.ImageField(upload_to='product')
