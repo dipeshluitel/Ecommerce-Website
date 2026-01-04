a7c2ad1bce752ab2e4877aeab24a7a5abca16ae8
Dipesh <dipeshluitel40@gmail.com>
2026-01-04 14:50:41 +0545

    create(models): product models has been created

--- ecommerce/goCart/models.py
+++ ecommerce/goCart/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = (
+    ('wc', 'Womens Clothes'),
+    ('mc', 'mens Clothes'),
+    ('cc', 'Children Clothes'),
+    ('k', 'Kitchen Items'),
+    ('t', 'Technology'),
+    ('f', 'Fashion'),
+)
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES , max_length=2)
+    product_image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.title
