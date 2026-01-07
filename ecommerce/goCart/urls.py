52b0fa2bda2350f88fdb0c4b9caf047c73a361bb
Dipesh <dipeshluitel40@gmail.com>
2026-01-07 07:35:13 +0545

    fix(ulrs): project and main urls fixed for displaying  media

--- ecommerce/goCart/urls.py
+++ ecommerce/goCart/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 app_name = 'goCart'
 
@@ -10,8 +9,3 @@ urlpatterns = [
     path('category/<slug:val>', views.CategoryView.as_view(), name='category'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
